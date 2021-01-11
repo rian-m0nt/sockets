5ab33fa162c9eae6bfe582fa3a4eb3e1c8fb353b
rian-uni <montgomeryrian@gmail.com>
2021-01-11 20:25:13 +0000

    First commit

---
+++ client.py
@@ -0,0 +1,10 @@
+import sys
+
+args = sys.argv[1:]
+
+hostname = args[0]
+port = args[1]
+operation = args[2]
+filename = args[3]
+
+
---
+++ utils.py
@@ -0,0 +1,17 @@
+
+
+def send_file(socket, filename):
+    print("Sending file...")
+
+
+def recv_file(socket, filename):
+    print("Receiving file...")
+
+
+def send_listing(socket):
+    print("Sending listing...")
+
+
+def recv_listing(socket):
+    print("Receiving listing...")
+

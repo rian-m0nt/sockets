b0c3cdd6c6108faf0196fde25c40dbcfcc4ee4ef
rian-uni <montgomeryrian@gmail.com>
2021-01-20 16:22:56 +0000

    Added try catch statements

--- utils.py
+++ utils.py
@@ -1,7 +1,5 @@
 import socket
-#Set the max timeout (corresponds to filesize, in this case no file over 100kb can be sent)
-MAX_TIMEOUT = 24
-DELIMITER=","
+
 
 def send_file(socket, filename):
     print("Sending file...")
@@ -10,8 +8,12 @@ def send_file(socket, filename):
     return sent_file
 
 def readfile(filename):
-    f = open(filename,"rb")
-    bytes=f.read()
+    try:
+        f = open(filename,"rb")
+        bytes=f.read()
+    except FileNotFoundError:
+        print(f"File {filename} not found.")
+
     return bytes
 
 
@@ -19,21 +21,19 @@ def recv_file(socket, filename):
     print("Receiving file...")
     data = bytearray(1)
     bytes_read=""
-    f = open("files/"+filename,"xb")
-    while(len(data) > 0 and "\\n" not in data.decode()):
-        data = socket.recv(4096)
-        print("Recieved data packet")
-        f.write(data)
-    f.close()
-    print("Completed recieve successfully");
+    try:
+        f = open("files/"+filename,"xb")
+        while(len(data) > 0 and "\\n" not in data.decode()):
+            data = socket.recv(4096)
+            print("Recieved data packet")
+            f.write(data)
+        f.close()
+        print("Completed recieve successfully");
+    except FileExistsError:
+        print(f"File {filename} already exists!")
     print(data);
 
 
-def save_to_file(data,filename):
-    print("Saving file")
-    f = open("storage/"+filename,"xb")
-    f.write(data)
-    f.close()
 
 def send_listing(socket):
     print("Sending listing...")
